552da91996458f329315121b8b04c1eab42646e7
Gary Lloyd M. Senoc <62071560+garysenoc@users.noreply.github.com>
2020-05-09 19:07:31 +0800

    Create mario.c

---
+++ mario.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int i,j,s,num,n;
+
+    do
+    	scanf("%d",&num);
+    while(num< 1 || num>8);
+    
+    
+    for(i=0;i<num;i++){
+		for(s=0;s<num-1-i;s++)
+			printf(" ");
+		for(j=0;j<(num+1+i)-num;j++)
+			printf("#");
+		printf("  ");
+		for(n=0;n<(num+1+i)-num;n++)
+			printf("#");
+		printf("\n");
+    }
+}
